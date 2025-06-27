51f2ee05ad586ff81737e39b5df160597b64049d
IldarSaygafarov2 <sayildar17@gmail.com>
2025-06-27 18:46:25 +0500

    added fixes

--- core/apps/categories/context.py
+++ core/apps/categories/context.py
@@ -1,9 +1,8 @@
 from django.http import HttpRequest
 
 from core.apps.categories.models import Category
-from core.apps.categories.schemas import CategorySchema
 
 
-def get_categories(request: HttpRequest) -> dict[str, list[CategorySchema]]:
-    categories = [category.to_entity() for category in Category.objects.all()]
+def get_categories(request: HttpRequest):
+    categories = Category.objects.all()
     return {'categories': categories}
